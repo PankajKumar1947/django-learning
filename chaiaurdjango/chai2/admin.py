bf24065f8e7e43ca963847ca44f6232bc4526737
pankajKumar1947 <pankaj.ky3007@gmail.com>
2025-02-27 23:40:41 +0530

    adding models in admin and customisizing the interface of  the admin

--- chaiaurdjango/chai2/admin.py
+++ chaiaurdjango/chai2/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 from .models import ChaiVariety, ChaiReview, ChaiCertificate, Store
 # Register your models here.
 
-admin.site.register(ChaiVariety)
+
+# Customizing the admin interface
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 1
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display= ('name', 'type', 'date_added')
+    inlines  = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'locatotion')
+    filter_horizontal = ('chai_varities',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number', 'issued_date', 'valid_until')
+
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(ChaiReview)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
+admin.site.register(Store, StoreAdmin)
